5c0e6637df405417a7652bddafa722eb39421dc4
市川　拓巳 <ichikawatakumi@ichikawatakumi-no-MacBook-Air.local>
2017-08-18 12:33:10 +0900

    けてぃーのゲーム

---
+++ 2d-shooting-game/Assets/Scripts/Emitter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Emitter : MonoBehaviour {
+  public GameObject[] waves;
+  private int currentWave;
+  private Manager manager;
+  
+  IEnumerator Start () {
+    if (waves.Length == 0) {
+      yield break;
+    }
+    manager = FindObjectOfType<Manager>();
+    while (true) {
+      while(manager.IsPlaying() == false) {
+        yield return new WaitForEndOfFrame ();
+      }
+      GameObject g = (GameObject)Instantiate (waves [currentWave], transform.position, Quaternion.identity);
+      g.transform.parent = transform;
+      while (g.transform.childCount != 0) {
+        yield return new WaitForEndOfFrame ();
+      }
+      Destroy (g);
+      if (waves.Length <= ++currentWave) {
+        currentWave = 0;
+      }
+    }
+  }
+}
---
+++ 2d-shooting-game/Assets/Scripts/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+  public int hp = 1;
+  Spaceship spaceship;
+  
+  IEnumerator Start () {
+    spaceship = GetComponent<Spaceship> ();
+    Move (transform.up * -1);
+    if (spaceship.canShot == false) {
+      yield break;
+    }
+    while (true) {
+      for (int i = 0; i < transform.childCount; i++) {
+        Transform shotPosition = transform.GetChild (i);
+        spaceship.Shot (shotPosition);
+      }
+      yield return new WaitForSeconds (spaceship.shotDelay);
+    }
+  }
+
+  public void Move (Vector2 direction) {
+    GetComponent<Rigidbody2D>().velocity = direction * spaceship.speed;
+  } 
+  
+  void OnTriggerEnter2D (Collider2D c) {
+    string layerName = LayerMask.LayerToName (c.gameObject.layer);
+    if (layerName != "Bullet (Player)") return;
+    Transform playerBulletTransform = c.transform.parent;
+    Bullet bullet =  playerBulletTransform.GetComponent<Bullet>();   
+    hp = hp - bullet.power;
+    Destroy(c.gameObject);
+    if(hp <= 0 ) {
+      spaceship.Explosion ();
+      Destroy (gameObject);
+    }else{
+      spaceship.GetAnimator().SetTrigger("Damage");
+    }
+  }
+}
